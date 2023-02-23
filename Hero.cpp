af26dc85d7ba6a82ca1e3a21e123d547dc5ef931
easweeney01 <103469000+easweeney01@users.noreply.github.com>
2023-02-23 17:48:10 -0500

    Hero can now walk

--- Hero.cpp
+++ Hero.cpp
@@ -5,9 +5,12 @@
 #include "WorldManager.h"
 
 Hero::Hero() {
+	hearts = 3;
+	inv = 30;
 
 	// Setup "saucer" sprite.
 	setSprite("heroR");
+	dir = 1; //1 is right, -1 is left
 
 	// Set object type.
 	setType("Hero");
@@ -18,13 +21,123 @@ Hero::Hero() {
 	// Set starting location in middle of window.                             
 	int world_horiz = (int)WM.getBoundary().getHorizontal();
 	int world_vert = (int)WM.getBoundary().getVertical();
-	df::Vector p(world_horiz / 4, world_vert / 4);
+	df::Vector p((float) world_horiz / 4, (float) world_vert / 4);
 	setPosition(p);
 	
+	setSolidness(df::HARD);
+
+	//Activates Interest
+	registerInterest(df::KEYBOARD_EVENT);
+	registerInterest(df::STEP_EVENT);
+	registerInterest(df::MSE_EVENT);
 }
 
-int Hero::eventHandler(const df::Event* p_e) {
+Hero::~Hero() {
 
+}
+
+//Mouse, Keyboard, and 
+int Hero::eventHandler(const df::Event* p_e) {
+	LM.writeLog("Event Recieved");
+	if (p_e->getType() == df::KEYBOARD_EVENT) {
+
+		const df::EventKeyboard* p_keyboard_event = dynamic_cast <const df::EventKeyboard*> (p_e);
+		kbd(p_keyboard_event);
+		return 1;
+	}
+
+	if (p_e->getType() == df::STEP_EVENT) {
+		LM.writeLog("Stepping");
+		step();
+		return 1;
+	}
+
+	if (p_e->getType() == df::MSE_EVENT) {
+		const df::EventMouse* p_mouse_event =
+			dynamic_cast <const df::EventMouse*> (p_e);
+		mouse(p_mouse_event);
+		return 1;
+	}
+
+	return 0;
+}
+
+void Hero::kbd(const df::EventKeyboard* p_keyboard_event) {
+	bool down = (p_keyboard_event->getKeyboardAction() == df::KEY_DOWN);
+	bool press = (p_keyboard_event->getKeyboardAction() == df::KEY_PRESSED);
+	bool release = (p_keyboard_event->getKeyboardAction() == df::KEY_RELEASED);
+
+	if (down) {
+		LM.writeLog("down");
+		switch (p_keyboard_event->getKey()) {
+		case df::Keyboard::W:       // up
+			moveY(-0.25);
+			break;
+
+		case df::Keyboard::S:       // down
+			moveY(0.25);
+			break;
+
+		case df::Keyboard::A:       // left
+			moveX(-0.5);
+
+			if (dir != -1) { setSprite("heroL"); }
+			dir = -1;
+			break;
+		case df::Keyboard::D:       // right
+			moveX(0.5);
+			if (dir != 1) { setSprite("heroR"); }
+			dir = 1;
+			break;
+		}
+	} else if (press) { //
+		LM.writeLog("press");
+		switch (p_keyboard_event->getKey()) {
+		case df::Keyboard::SPACE:       //Space (Time Stop)
+			LM.writeLog("ZA WARUDO!");
+			break;
+		}
+	}
+
+	return;
+}
+
+void Hero::mouse(const df::EventMouse* p_mouse_event) {
+
+}
+
+void Hero::moveX(float dx) {
+	// If stays on window, allow move.
+	df::Vector new_pos(getPosition().getX() + dx, getPosition().getY());
+	if ((new_pos.getX() > 3) && (new_pos.getX() < WM.getBoundary().getHorizontal() - 1))
+		WM.moveObject(this, new_pos);
+		LM.writeLog("Attempting to move");
+
+	// If stays on window, allow move.
+	if ((new_pos.getX() > 3) && (new_pos.getX() < WM.getBoundary().getHorizontal()))
+		WM.moveObject(this, new_pos);
+		LM.writeLog("Attempting to move");
+}
+
+void Hero::moveY(float dy) {
+	// If stays on window, allow move.
+	df::Vector new_pos(getPosition().getX(), getPosition().getY() + dy);
+	if ((new_pos.getY() > 3) &&
+		(new_pos.getY() < WM.getBoundary().getVertical() - 1))
+		WM.moveObject(this, new_pos);
+
+	// If stays on window, allow move.
+	if ((new_pos.getY() > 3) && (new_pos.getY() < WM.getBoundary().getVertical()))
+		WM.moveObject(this, new_pos);
+}
+
+void Hero::step() {
+	if (inv > 0) {
+		inv--;
+	}
+
+	//df::Vector v = df::Vector(10,1);
+	//this->moveTo(v);
 }
 
 //Hearts and Invincible Frames
--- Hero.h
+++ Hero.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include "Object.h"
+
 #include "Event.h"
-#include <EventMouse.h>
+#include "EventMouse.h"
+#include "EventKeyboard.h"
+#include "EventStep.h"
 
 class Hero : public df::Object {
 	private:
 		int hearts;
 		int inv;
+		int dir;
+
 	public:
 		Hero();
 		~Hero();
@@ -19,8 +24,12 @@ class Hero : public df::Object {
 		void setInv(int newI);
 
 		int eventHandler(const df::Event* p_e) override;
-		void Hero::mouse(const df::EventMouse* p_mouse_event);
+		void mouse(const df::EventMouse* p_mouse_event);
 		void kbd(const df::EventKeyboard* p_keyboard_event);
+		void step();
+
+		void moveX(float dy);
+		void moveY(float dy);
 };
 
 
